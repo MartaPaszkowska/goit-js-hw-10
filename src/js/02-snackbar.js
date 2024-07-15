8fe06ada0ce9d11166251bba23b7f1aa19f65cfa
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-07-15 20:17:24 +0200

    snackbar done

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,4 +1,45 @@
-'use strict';
-////////biblioteki/////////////
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('form[data-form]');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delayInput = form.elements['delay'];
+  const stateInput = form.elements['state'];
+  const delay = delayInput.value.trim();
+  const state = stateInput.value.trim();
+
+  createPromise(parseInt(delay), state)
+    .then(delay => {
+      iziToast.success({
+        icon: 'none',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#95EC3E',
+        messageColor: 'white',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        icon: 'none',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#EC483E',
+        messageColor: '#ffffff',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
